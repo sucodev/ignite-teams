e49de6fd8d6809003247d04e90efca5f7b2e64c2
sucodev <sucomanso@gmail.com>
2023-04-12 21:04:47 -0300

    test commit IIII

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import { Loading } from "@components/Loading";
 
 import { Routes } from './src/routes';
 
-// test comment III
+// test comment IIII
 export default function App() {
 
   const [fontsLoaded] = useFonts({
